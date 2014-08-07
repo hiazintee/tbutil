50637addc96c021b785cea496c5afafb506ad681
Hiazintee <bernd.wiedmann@gmx.net>
2014-08-07 20:32:21 +0200

    added first go source files (stubs more or less)

---
+++ tbscreen.go
@@ -0,0 +1,13 @@
+package bsq
+
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"strconv"
+)
+
+struct tbscreen {
+   height, width int
+   title string
+   data []string
+}
---
+++ tbtutil.go
@@ -0,0 +1,39 @@
+package bsq
+
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"strconv"
+)
+
+type tbScreenPoint struct {
+	tbcell termbox.Cell
+	Line, Column int
+}
+
+func (p tbScreenPoint*) GetRune() rune {
+	return p.tbcell.Ch
+}
+
+type tbscreen_data struct {
+	Cell                                           []termbox.Cell
+	visibleStarline, visibleCount, visibleMaxWidth int
+}
+
+type tbscreen struct {
+	height, width int
+	title         string
+	data          tbscreen_data
+}
+
+func tbDrawCell(line, column int, c rune) {
+	termbox.SetCell(column, line, c, termbox.ColorDefault, termbox.ColorDefault)
+}
+
+func tbDrawLine(line int, data string) {
+	col := 0
+	for _, x := range data {
+		tbDrawCell(line, col, x)
+		col++
+	}
+}
